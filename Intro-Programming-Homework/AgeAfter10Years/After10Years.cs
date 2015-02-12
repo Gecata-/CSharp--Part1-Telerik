d06e5f901154259546cb48dacc34b4e52e79fdd4
Georgi Dimitrov <gb_dimitrov@yahoo.com>
2015-02-12 16:11:42 +0200

    Intro to programning

---
+++ Intro-Programming-Homework/AgeAfter10Years/After10Years.cs
@@ -0,0 +1,19 @@
+﻿//Problem 15.* Age after 10 Years
+//
+//   Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
+using System;
+
+
+class Program
+{
+    static void Main();
+
+        {
+        int i = 3/18/1987;
+        
+            Console.WriteLine("I`m (i + 1/14/2015) ");
+        }
+    
+}
+
---
+++ Intro-Programming-Homework/PrintLongSequence/PrintLongSequence.cs
@@ -0,0 +1,20 @@
+﻿// Problem 16.* Print Long Sequence
+//
+//    Write a program that prints the first 1000 members of the sequence: 2, -3, 4, -5, 6, -7, …
+//    You might need to learn how to use loops in C# (search in Internet).
+
+using System;
+
+class LongSequence
+{
+    static void Main()
+    {
+        for (int i = 2 ; i <= 1000; )
+        {
+            Console.WriteLine(i++);
+            Console.WriteLine(-i++);
+
+        }
+    }
+}
+
